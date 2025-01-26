001718cebc75e95f9b43a791b07574186f122ef5
George <drugovh@mail.ru>
2025-01-26 14:48:34 +0300

    add filters for html parser

--- src/main/java/searchengine/services/HtmlParser.java
+++ src/main/java/searchengine/services/HtmlParser.java
@@ -8,10 +8,43 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HtmlParser {
+    private static final List<String> IMAGE_EXTENSIONS = List.of(
+            ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg", ".webp", ".tiff"
+    );
+
+    private static final List<String> VIDEO_EXTENSIONS = List.of(
+            ".mp4", ".avi", ".mkv", ".mov", ".webm"
+    );
+
+    private static final List<String> AUDIO_EXTENSIONS = List.of(
+            ".mp3", ".wav", ".ogg"
+    );
+
+    private static final List<String> DOCUMENT_EXTENSIONS = List.of(
+            ".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".txt"
+    );
+
+    private static final List<String> ARCHIVE_EXTENSIONS = List.of(
+            ".zip", ".rar", ".7z", ".tar", ".gz"
+    );
+
+    private static final List<String> EXECUTABLE_EXTENSIONS = List.of(
+            ".exe", ".bin", ".msi", ".sh"
+    );
+
+    private static final List<String> SCRIPT_AND_STYLE_EXTENSIONS = List.of(
+            ".js", ".css"
+    );
+
+    private static final List<String> UNWANTED_KEYWORDS = List.of(
+            "javascript:void", "#"
+    );
     private final String url;
 
     public HtmlParser(String url) {
@@ -27,13 +60,8 @@ public class HtmlParser {
                 .map(link -> link.attr("abs:href"))
                 .filter(this::isValidUrl)
                 .filter(href -> isSameDomain(href, baseURI))
-                .filter(href -> !containsPdfKeyword(href))
+                .filter(this::isHtmlPage)
                 .collect(Collectors.toSet());
-    }
-
-
-    private boolean containsPdfKeyword(String url) {
-        return url.toLowerCase().contains("pdf");
     }
 
     private boolean isValidUrl(final String url) {
@@ -48,6 +76,28 @@ public class HtmlParser {
             throw new RuntimeException(e);
         }
         return childURI.getHost().contains(baseURI.getHost());
+    }
+
+    private boolean isHtmlPage(String url) {
+        return !hasUnwantedExtension(url) && !containsUnwantedKeywords(url);
+    }
+
+    private boolean hasUnwantedExtension(String url) {
+        String lowerCaseUrl = url.toLowerCase();
+        return Stream.of(
+                IMAGE_EXTENSIONS,
+                VIDEO_EXTENSIONS,
+                AUDIO_EXTENSIONS,
+                DOCUMENT_EXTENSIONS,
+                ARCHIVE_EXTENSIONS,
+                EXECUTABLE_EXTENSIONS,
+                SCRIPT_AND_STYLE_EXTENSIONS
+        ).flatMap(List::stream).anyMatch(lowerCaseUrl::endsWith);
+    }
+
+    private boolean containsUnwantedKeywords(String url) {
+        String lowerCaseUrl = url.toLowerCase();
+        return UNWANTED_KEYWORDS.stream().anyMatch(lowerCaseUrl::contains);
     }
 }
 
